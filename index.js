9dee005b63b2c9ea0e88a9da5e45dd86546b6263
A <Arsema.arsemaagebremichael@gmail.com>
2025-05-02 11:54:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function who_moved(){
+    window.location.href = 'https://medium.com/@joaomoliveira.gomes/who-moved-my-cheese-by-dr-spencer-johnson-book-review-f1adba4ecec3';
+  
+  }
+  
+  function readNow(){
+    window.location.href = 'https://pursuitofdiversity.wordpress.com/wp-content/uploads/2018/06/born-a-crime-trevor-noah.pdf';
+  
+  }
