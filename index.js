e20b5133cd0ac72bcbcbe45b2ba28cf41d3347c8
A <Arsema.arsemaagebremichael@gmail.com>
2025-05-02 15:57:33 +0300

    adding js files

--- index.js
+++ index.js
@@ -4,11 +4,34 @@ function who_moved(){
   
   }
   
-  function readNow(){
+  function home(){
+    window.location.href = '#curated_list';
+  
+  }
+  function homegoing(){
+    window.location.href = '  https://downloads.bbc.co.uk/radio4/open-book/9780241242735_homegoing_txt.pdf';
+  
+  }
+  function born_crime(){
     window.location.href = 'https://pursuitofdiversity.wordpress.com/wp-content/uploads/2018/06/born-a-crime-trevor-noah.pdf';
   
   }
-  function home(){
-    window.location.href = 'trevor_blog.html';
+  function nineteen(){
+    window.location.href = 'https://www.planetebook.com/free-ebooks/1984.pdf';
   
   }
+ 
+  function things(){
+    window.location.href = ' https://www.centralschool.edu.vu/uploads/1/1/4/4/114402701/things-fall-apart.pdf';
+  
+  }
+
+  function hunger(){
+    window.location.href = ' https://online.fliphtml5.com/elbz/nodb/#p=1';
+  
+  }
+  function things(){
+    window.location.href = ' https://www.centralschool.edu.vu/uploads/1/1/4/4/114402701/things-fall-apart.pdf';
+  
+  }
+
